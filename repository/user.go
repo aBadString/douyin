ab1d8f1f2c79c03cc6ef9868e168719a6ce9cadd
peadx <1823685163@qq.com>
2023-02-05 17:47:33 +0800

    关注/取关增加事务

--- repository/user.go
+++ repository/user.go
@@ -29,17 +29,36 @@ func GetUserById(userId int) User {
 	return user
 }
 
-func UpdateUserCount(currentUserId, toUserId, mode int) {
-	if mode == 1 {
-		ORM.Model(&User{Id: currentUserId}).
-			Update("follow_count", gorm.Expr("follow_count+?", 1))
-		ORM.Model(&User{Id: toUserId}).
-			Update("follower_count", gorm.Expr("follower_count+?", 1))
-	}
-	if mode == 2 {
-		ORM.Model(&User{Id: currentUserId}).
-			Update("follow_count", gorm.Expr("follow_count-?", 1))
-		ORM.Model(&User{Id: toUserId}).
-			Update("follower_count", gorm.Expr("follower_count-?", 1))
-	}
+func UpdateUserCount(currentUserId, toUserId, mode int) error {
+
+	var err error
+	return ORM.Transaction(func(tx *gorm.DB) error {
+		// 在事务中执行一些 db 操作（从这里开始，您应该使用 'tx' 而不是 'db'）
+		if mode == 1 {
+			err = tx.Model(&User{Id: currentUserId}).
+				Update("follow_count", gorm.Expr("follow_count+?", 1)).Error
+			if err != nil {
+				return err
+			}
+			err = tx.Model(&User{Id: toUserId}).
+				Update("follower_count", gorm.Expr("follower_count+?", 1)).Error
+			if err != nil {
+				return err
+			}
+			return nil
+
+		} else {
+			err = tx.Model(&User{Id: currentUserId}).
+				Update("follow_count", gorm.Expr("follow_count-?", 1)).Error
+			if err != nil {
+				return err
+			}
+			err = tx.Model(&User{Id: toUserId}).
+				Update("follower_count", gorm.Expr("follower_count-?", 1)).Error
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+	})
 }
